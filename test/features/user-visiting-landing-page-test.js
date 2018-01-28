5290d4154d2d469d2250b9a2b539c59792537306
JessC Jess <jessicacornejojara@gmail.com>
2018-01-27 18:09:28 -0800

    Added test -> feature -> user visits landing page

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,10 @@
 const {assert} = require('chai');
+
+describe('User hits main page ->', () => {
+  describe('with no videos added yet ->', () => {
+    it('page has no items', () => {
+      browser.url('/');
+      assert.equal(browser.getText('#videos-container'), '')
+    })
+  })
+})
